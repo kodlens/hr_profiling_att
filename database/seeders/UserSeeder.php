eb2031a21f66629905d807338480126674a6757b
etienne <etiennewayne@gmail.com>
2023-11-16 13:08:03 +0800

    adding new roles

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -35,7 +35,7 @@ class UserSeeder extends Seeder
 
                 'email' => 'admin@dev.com',
                 'contact_no' => '09167789585',
-                'role' => 'ADMINISTRATOR',
+                'role' => 'administrator',
                 'password' => Hash::make('a')
             ],
 
@@ -59,9 +59,58 @@ class UserSeeder extends Seeder
 
                 'email' => 'junrey@dev.com',
                 'contact_no' => '09683014598',
-                'role' => 'EMPLOYEE',
+                'role' => 'employee',
                 'password' => Hash::make('a')
             ],
+
+            [
+                'username' => 'cj',
+                'lname' => 'LENOGON',
+                'fname' => 'CJ KEN',
+                'mname' => 'P',
+                'sex' => 'MALE',
+
+                'res_province' => 'MISAMIS OCCIDENTAL',
+                'res_city' => 'TANGUB CITY',
+                'res_barangay' => 'CANIANGAN',
+                'res_street' => 'P-6',
+
+                'per_province' => 'MISAMIS OCCIDENTAL',
+                'per_city' => 'TANGUB CITY',
+                'per_barangay' => 'CANIANGAN',
+                'per_street' => 'P-6',
+
+                'email' => 'cj@dev.com',
+                'contact_no' => '09683014598',
+                'role' => 'point_person',
+                'password' => Hash::make('a')
+            ],
+
+
+            [
+                'username' => 'eman',
+                'lname' => 'FUENTES',
+                'fname' => 'EMMANUEL',
+                'mname' => 'P',
+                'sex' => 'MALE',
+
+                'res_province' => 'MISAMIS OCCIDENTAL',
+                'res_city' => 'TANGUB CITY',
+                'res_barangay' => 'CANIANGAN',
+                'res_street' => 'P-6',
+
+                'per_province' => 'MISAMIS OCCIDENTAL',
+                'per_city' => 'TANGUB CITY',
+                'per_barangay' => 'CANIANGAN',
+                'per_street' => 'P-6',
+
+                'email' => 'eman@dev.com',
+                'contact_no' => '09683014598',
+                'role' => 'training_dev_officer',
+                'password' => Hash::make('a')
+            ],
+
+
         ];
 
         \App\Models\User::insertOrIgnore($data);
