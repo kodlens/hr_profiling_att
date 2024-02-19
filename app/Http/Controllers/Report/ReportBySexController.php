78a2d6982159a022b7a3fb701678b3172ef50b1d
etiennewayne <etiennewayne@gmail.com>
2024-02-19 21:05:36 +0800

    temporary update

--- app/Http/Controllers/Report/ReportBySexController.php
+++ app/Http/Controllers/Report/ReportBySexController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Report;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Support\Facades\DB;
 
 class ReportBySexController extends Controller
 {
@@ -17,8 +18,14 @@ class ReportBySexController extends Controller
 
     public function loadReportBySex(Request $req){
         $designation = $req->designation == '' || $req->designation == null ? '' : $req->designation;
-        $data = User::where('designation', 'like', $designation . '%')
-            ->get();
+        
+        $data = DB::select('
+            select
+            (select count(sex) from users where sex = `MALE`) as sum_male,
+            (select count(sex) from users where sex = `FEMALE`) as sum_female
+            from
+            users where users.designation like ?
+        ', [$designation . '%']);
 
         return $data;
     }
