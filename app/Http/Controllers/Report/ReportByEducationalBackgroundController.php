480d6c452523bfdf0defc58fc4660af75fd76c64
etienne <etiennewayne@gmail.com>
2024-06-03 14:18:18 +0800

    update query ed bg

--- app/Http/Controllers/Report/ReportByEducationalBackgroundController.php
+++ app/Http/Controllers/Report/ReportByEducationalBackgroundController.php
@@ -14,14 +14,33 @@ class ReportByEducationalBackgroundController extends Controller
     }
 
     public function loadReportEducationalBackground(Request $req){
-        $data = EducationalBackground::select('educational_backgrounds.level')
-            ->selectRaw('COUNT(educational_backgrounds.level) AS count_level')
-            ->join('users', 'users.user_id', '=', 'educational_backgrounds.user_id')
-            ->groupBy('educational_backgrounds.level')
-            ->orderByRaw("FIELD(educational_backgrounds.level, 'ELEMENTARY', 'SECONDARY', 'VOCATIONAL/TRADE COURSE', 'COLLEGE', 'GRADUATE STUDIES', 'POST GRADUATE')")
+
+
+        // $data = EducationalBackground::select('educational_backgrounds.level')
+        //     ->selectRaw('COUNT(educational_backgrounds.level) AS count_level')
+        //     ->join('users', 'users.user_id', '=', 'educational_backgrounds.user_id')
+        //     ->groupBy('educational_backgrounds.level')
+        //     ->orderByRaw("FIELD(educational_backgrounds.level, 'ELEMENTARY', 'SECONDARY', 'VOCATIONAL/TRADE COURSE', 'COLLEGE', 'GRADUATE STUDIES', 'POST GRADUATE')")
             
-            ->where('users.designation', 'like', strtolower($req->designation) . '%')
-            ->get();
+        //     ->where('users.designation', 'like', strtolower($req->designation) . '%')
+        //     ->get();
+
+        $data = \DB::select('
+            SELECT 
+            `level`,
+            `designation`,
+            COUNT(`level`) AS count_level FROM (SELECT
+            a.user_id,
+            a.level,
+            b.designation
+            FROM
+            educational_backgrounds a
+            JOIN users AS b ON a.user_id = b.user_id
+            WHERE b.designation LIKE ?
+            GROUP BY a.user_id, a.level) AS aa
+            GROUP BY aa.level
+            ORDER BY FIELD(aa.level, "ELEMENTARY", "SECONDARY", "VOCATIONAL/TRADE COURSE", "COLLEGE", "GRADUATE STUDIES", "POST GRADUATE")
+        ', [$req->designation . '%']);
 
         return $data;
     }
